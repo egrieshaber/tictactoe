6f7cb9bed66ff71089352231ca8f372779d16ebe
egrieshaber <eric.j.grieshaber>
2018-11-01 11:38:43 -0400

    third commit.
    11/1/18 11:39am .. added functionality.  still errors

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,10 +14,12 @@ $(() => {
   let movesMade = 0
 
   const box = $('.box')
+  const winnerContainer = $('.winner')
+  const reset = $('.reset')
 
   box.on('click', function () {
     movesMade++
-    if (currentTurn === 1) {
+    if (currentTurn % 2 === 1) {
       event.target.innerHTML = playerOne
       event.target.style.color = 'red'
       currentTurn++
@@ -26,7 +28,60 @@ $(() => {
       event.target.style.color = 'blue'
       currentTurn--
     }
+    if (checkForWinner()) {
+      const theWinner = currentTurn === 1 ? playerTwo : playerOne
+      declareWinner(theWinner)
+    }
   })
+
+  reset.on('click', function (e) {
+    const moves = Array.prototype.slice.call($('.inner'))
+    moves.map((m) => {
+      m.innerHTML = ''
+    })
+    winnerContainer.html('')
+    winnerContainer.css('display', 'none')
+    currentTurn = 1
+    movesMade = 0
+  })
+
+  function checkForWinner () {
+    if (movesMade > 4) {
+      const moves = Array.prototype.slice.call($('.inner'))
+      const results = moves.map(function (box) {
+        return box.innerHTML
+      })
+
+      const winCombo = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ]
+      return winCombo.find(function (combo) {
+        if (results[combo[0]] !== '' &&
+          results[combo[1]] !== '' &&
+          results[combo[2]] !== '' &&
+          results[combo[0]] === results[combo[1]] && results[combo[1]] ===
+          results[combo[2]]) {
+          return true
+        } else {
+          return false
+        }
+      })
+    }
+  }
+
+  function declareWinner (winner) {
+    winnerContainer.css('display', 'block')
+    reset.css('display', 'block')
+    winner = winner === playerOne ? 'X' : 'O'
+    winnerContainer.html(winner + 'Wins!')
+  }
 
   // $('.inner').on('click', function () {
   //
